73ec3ff3fc2a43e0da5910f59e60fe17d3257d9c
Katerina <ekaterina.elistratova@biocen.ag>
2026-03-29 21:37:58 +0200

    serve html page; refactoring

--- server.py
+++ server.py
@@ -1,37 +1,57 @@
 import socket
+import os
 
 HOST = "127.0.0.1"
 PORT = 8080
 MAX_DATA_SIZE = 1024
+WWW_ROOT = "www"
 
-# Create a TCP socket to listen on localhost:80.
-# Wait for a connection from a client.
-# Receive the HTTP request, parse the first line.
-# Send a minimal HTTP response with the requested path.
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-try:
-    s.bind((HOST, PORT))
+def setup_server(host, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, port))
     s.listen(1)
     print(f"Server running on {HOST}: {PORT}...")
+    return s
 
+
+def handle_request(conn):
+    data = conn.recv(MAX_DATA_SIZE)
+    request_line = data.split(b'\r\n')[0].decode()
+    method, path, version = request_line.split()
+    print(f"Request: {method} {path} {version}")
+
+    if path == "/":
+        path = "/index.html"
+    file_path = os.path.join(WWW_ROOT, path.lstrip("/"))
+    if os.path.exists(file_path):
+        with open(file_path, "r", encoding="utf-8") as f:
+            body = f.read()
+            response = f"HTTP/1.1 200 OK\r\n\r\n{body}\r\n"
+    else:
+        response = "HTTP/1.1 404 Not Found\r\n\r\nPage Not Found"    
+    conn.sendall(response.encode())
+
+
+def handle_connection(server_socket):
     while True:
-        conn, addr = s.accept()
+        conn, addr = server_socket.accept()
         print('Connected by', addr)
 
         try:
-            data = conn.recv(MAX_DATA_SIZE)
-            request_line = data.split(b'\r\n')[0].decode()
-            method, path, version = request_line.split()
-            print(f"Request: {method} {path} {version}")
-
-            response = f"HTTP/1.1 200 OK\r\n\r\nRequested path: {path}\r\n"
-            conn.sendall(response.encode())
-
+            handle_request(conn)
         finally:
             # close client socket
             conn.close()
-finally:
-    # close server socket
-    s.close()
+
+def main():
+    server_socket = setup_server(HOST, PORT)
+    try:
+        handle_connection(server_socket)
+    finally:
+         # close server socket
+        server_socket.close()
+
+
+if __name__ == "__main__":
+    main()
